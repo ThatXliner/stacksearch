df64271ac26b3de0c56916991f6239bfa58fe19a
theXliner <bryanhu@theXliners-Mac.hsd1.ca.comcast.net>
2020-07-21 18:37:00 -0700

    Got it functional

--- main.py
+++ main.py
@@ -19,6 +19,45 @@ import sys
 import requests
 from bs4 import BeautifulSoup as bs
 
+
+def Search(Query: str) -> dict:
+    """For getting very precise information on StackOverflow.
+
+    Returns
+    -------
+    dict
+        Description of returned object.
+
+    """
+    r = requests.get(f"https://stackoverflow.com/search?q={Query}")
+    soup = bs(r.content, "lxml")
+    questions = {  # The raw ingredients
+        question.string: question.get("href")
+        for question in soup.find_all(
+            attrs={"class": "question-hyperlink", "data-gps-track": None}
+        )
+    }
+    pages = [  # Pages of all the questions related to Query
+        bs(requests.get(link).content, "lxml")
+        for link in map(
+            lambda x: "https://stackoverflow.com" + x, iter(questions.values())
+        )
+    ]
+    full_questions = [
+        page.find(attrs={"class": "post-text", "itemprop": "text"}).get_text()
+        for page in pages
+    ]
+    answers = [
+        [
+            answer.get_text()
+            for answer in page.find_all(attrs={"itemtype": "http://schema.org/Answer"})
+        ]
+        for page in pages
+    ]
+    output = dict(zip(full_questions, answers))
+    return output
+
+
 parser = argparse.ArgumentParser(
     prog="StackSearch",
     formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -30,6 +69,10 @@ thing. The reason you should use this is because this returns results that you c
 use. If ran from the command line, it'll return human readable results. If ran from
 another python script, it'll return some parsable JSON. Assuming you are utilizing
 this script's wonderful functions and objects.""",
-    epilog='\nJudge a man by his questions rather than by his answers" - Voltaire\n',
+    epilog=' \n Judge a man by his questions rather than by his answers" - Voltaire \n ',
 )
-parser.print_help()
+parser.add_argument("query", type=str, help="The query to search", nargs="+")
+# parser.add_argument("-o", "--output", type=str, help="The output file", nargs="?")
+args = parser.parse_args(sys.argv[1:])
+
+print(Search(args.query))
