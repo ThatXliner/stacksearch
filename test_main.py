1f8d6ef23d63bc91f933c49cc00690bbde7c28f0
ThatXliner <66848002+ThatXliner@users.noreply.github.com>
2020-07-23 10:17:10 -0700

    Added Sites option to async Search and Search

--- test_main.py
+++ test_main.py
@@ -26,7 +26,7 @@ NEWLINE = "\n"
 
 
 async def fSearch(
-    Query: str, print_prog: bool = True, *args: Any, **kwargs: Any
+    Query: str, print_prog: bool = True, sites: list = ["stackoverflow.com"],*args: Any, **kwargs: Any
 ) -> dict:
     """For getting very precise information on StackOverflow.
     This is 'supposed' to be faster than the normal 'Search' function for it abuses
@@ -63,20 +63,22 @@ async def fSearch(
         return [  # Pages of all the questions related to Query
             bs(link.content, "lxml") for link in _links_for_pages
         ]
-
+    async def get_stuff(sites, Query):
+        for site in sites:
+            yield requests.get(f"https://{site}/search?q={Query}")
     if print_prog:
         print("Requesting results from StackOverflow...")
-    r = requests.get(f"https://stackoverflow.com/search?q={Query}")
+    responses = get_stuff(sites, Query)
     if print_prog:
         print("Parsing response HTML...")
-    soup = bs(r.content, "lxml")
+    soups = (bs(r.content, "lxml") for r in responses)
     if print_prog:
         print("Collecting question links...")
     questions = {  # The raw ingredients
         question.string: question.get("href")
         for question in soup.find_all(
             attrs={"class": "question-hyperlink", "data-gps-track": None}
-        )
+        ) for soup in soups
     }
     if print_prog:
         print("Requesting questions found (This may take a while)...")
@@ -97,7 +99,7 @@ async def fSearch(
     return dict(zip(full_questions, answers))
 
 
-def Search(Query: str, print_prog: bool = True, *args: Any, **kwargs: Any) -> dict:
+def Search(Query: str, print_prog: bool = True, sites: list = ["stackoverflow.com"],*args: Any, **kwargs: Any) -> dict:
     """For getting very precise information on StackOverflow. This is the function you should use.
     Returns
     -------
@@ -107,17 +109,17 @@ def Search(Query: str, print_prog: bool = True, *args: Any, **kwargs: Any) -> di
     TEXT_REQUIREMENTS = {"class": "post-text", "itemprop": "text"}
     if print_prog:
         print("Requesting results from StackOverflow...")
-    r = requests.get(f"https://stackoverflow.com/search?q={Query}")
+    responses = (requests.get(f"https://{site}/search?q={Query}") for site in sites)
     if print_prog:
         print("Parsing response HTML...")
-    soup = bs(r.content, "lxml")
+    soups = (bs(r.content, "lxml") for r in responses)
     if print_prog:
         print("Collecting question links...")
     questions = {  # The raw ingredients
         question.string: question.get("href")
         for question in soup.find_all(
             attrs={"class": "question-hyperlink", "data-gps-track": None}
-        )
+        ) for soup in soups
     }
     if print_prog:
         print("Requesting questions found (This may take a while)...")
