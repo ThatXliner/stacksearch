e2c3f50699140ccc00f7872b1bdb77638ded2ea8
ThatXliner <66848002+ThatXliner@users.noreply.github.com>
2020-07-24 10:43:40 -0700

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -279,7 +279,7 @@ parser.add_argument(
 class TestClass:
     """For testing."""
 
-    def main(self, args, asynct: bool = False):
+    async def a_main(self, args):
         args = parser.parse_args((args).split())
         if args.version:
             print(f"stacksearch version: {__version__}")  # noqa
@@ -290,18 +290,11 @@ class TestClass:
             print(f"Searching {', '.join(SITES_TO_SEARCH)}...")
         ANSWERS = []
         for site in map(str, SITES_TO_SEARCH):
-                  if asynct:
-                    ANSWERS.append(
-                        await fSearch(
-                            " ".join(args.query), print_prog=PRINT_PROGRESS, search_on_site=site
-                        )
-                    )
-                  else:
-                    ANSWERS.append(
-                        Search(
-                            " ".join(args.query), print_prog=PRINT_PROGRESS, search_on_site=site
-                        )
-                    )
+            ANSWERS.append(
+                await fSearch(
+                    " ".join(args.query), print_prog=PRINT_PROGRESS, search_on_site=site
+                )
+            )
 
         if args.json:
             pprint(
@@ -347,7 +340,68 @@ class TestClass:
                         print("\n\n\n", file=args.OUTPUT)
                     finally:
                         question_number += 1
-    
+                  
+    def main(self, args):
+        args = parser.parse_args((args).split())
+        if args.version:
+            print(f"stacksearch version: {__version__}")  # noqa
+            sys.exit(0)
+        PRINT_PROGRESS = not args.s
+        SITES_TO_SEARCH = set(map(_remove_dot_com, args.sites))
+        if PRINT_PROGRESS:
+            print(f"Searching {', '.join(SITES_TO_SEARCH)}...")
+        ANSWERS = []
+        for site in map(str, SITES_TO_SEARCH):
+                ANSWERS.append(
+                    Search(
+                        " ".join(args.query), print_prog=PRINT_PROGRESS, search_on_site=site
+                    )
+                )
+
+        if args.json:
+            pprint(
+                ANSWERS, stream=args.OUTPUT, width=79
+            )  # You will get unprocessed, raw JSON
+        else:  # We got some parsing to do
+            if PRINT_PROGRESS:
+                print("Outputting results")
+
+            for answer in ANSWERS:
+                question_number = 1
+                print(t.bold("Answers from {}"))
+                for question, answers in answer.items():
+                    print(
+                        f"{t.bold}{t.bright_green}Question #{question_number}: {question}{t.normal}",
+                        file=args.OUTPUT,
+                    )
+                    print("\n")
+                    try:
+                        print(
+                            f"{t.bright_yellow}{t.bold} Best Answer: {answers[0]}{t.normal}",
+                            file=args.OUTPUT,
+                        )
+                        print("\n\n\n", file=args.OUTPUT)
+                        try:
+                            for question_answer in answers[1:]:
+                                print(
+                                    f"{t.green}Answer: {question_answer}{t.normal}",
+                                    file=args.OUTPUT,
+                                )
+                                print("\n\n\n", file=args.OUTPUT)
+                        except IndexError:
+                            print(
+                                f"{t.red}{t.bold}This is the only answer.{t.normal}",
+                                file=args.OUTPUT,
+                            )
+                    except IndexError:
+                        print(
+                            f"{t.bright_red}There were no answers for this question{t.normal}\n",
+                            file=args.OUTPUT,
+                        )
+                    else:
+                        print("\n\n\n", file=args.OUTPUT)
+                    finally:
+                        question_number += 1
     def test_one(self):
         """A test with Search."""
         self.main("python list")
@@ -355,7 +409,7 @@ class TestClass:
 #     @pytest.mark.asyncio
 #     async def test_two(self):
 #         """A test with the asyncio version of Search."""
-#         self.main("python list", asynct = True)
+#         self.a_main("python list")
 
     def test_one_lots_of_sites(self):
         """A test with Search. For lots of sites."""
@@ -364,4 +418,4 @@ class TestClass:
 #     @pytest.mark.asyncio
 #     async def test_two_lots_of_sites(self):
 #         """A test with the asyncio version of Search. For lots of sites."""
-#           self.main("python list --sites superuser.com stackoverflow", asynct = True)
+#           self.a_main("python list --sites superuser.com stackoverflow")
