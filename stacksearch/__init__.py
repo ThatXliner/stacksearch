abe5ee0c849a93e0be6a30628d43cded2a842167
theXliner <bryanhu@theXliners-Mac.hsd1.ca.comcast.net>
2020-07-24 16:21:54 -0700

    pypi!

--- stacksearch/__init__.py
+++ stacksearch/__init__.py
@@ -10,6 +10,6 @@ parsing, then returns a python dict containing questions and answers.
 """
 from .__main__ import main  # noqa: F401
 
-__version__ = "1.0.5"
+__version__ = "1.1.0"
 
 __author__ = ["Bryan Hu"]
