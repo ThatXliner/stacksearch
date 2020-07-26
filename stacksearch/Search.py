9d8954e5143ad284cad6f737d15c5b698b5d9d49
theXliner <bryanhu@theXliners-Mac.hsd1.ca.comcast.net>
2020-07-26 14:43:03 -0700

    Please work

--- stacksearch/Search.py
+++ stacksearch/Search.py
@@ -180,7 +180,7 @@ async def fSearch(
         questions = await findQuestions(soup)
         if print_prog:
             print("Requesting questions found (This may take a while)...")
-        _links_for_pages = [
+        _links_for_pages = [  # We cannot have a generator
             await client.get(link)
             for link in map(
                 lambda x: f"https://{search_on_site}.com" + x, iter(questions.values())
