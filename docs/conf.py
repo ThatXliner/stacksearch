25cca1eeef80c365882928821cf07f811b3703f0
theXliner <bryanhu@theXliners-Mac.hsd1.ca.comcast.net>
2020-07-26 21:33:56 -0700

    Update to 1.2.7.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Bryan Hu"
 author = "Bryan Hu"
 
 # The full version, including alpha/beta/rc tags
-release = "1.2.7.alpha"
+release = "1.2.7.1.alpha"
 
 
 # -- General configuration ---------------------------------------------------
