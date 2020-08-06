3a3dbd779878c01d31ab8a206987d2dda1776c51
theXliner <bryan.hu.cn@gmail.com>
2020-08-06 11:49:38 -0700

    Merged

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Bryan Hu"
 author = "Bryan Hu"
 
 # The full version, including alpha/beta/rc tags
-release = "1.2.10.dev1.alpha"
+release = "1.2.12.dev1.alpha"
 
 
 # -- General configuration ---------------------------------------------------
--- stacksearch/__init__.py
+++ stacksearch/__init__.py
@@ -9,6 +9,6 @@ parsing, then returns a python dict containing questions and answers.
 
 """
 
-__version__ = "1.2.10.dev1"
+__version__ = "1.2.12.dev1"
 
 __author__ = ["Bryan Hu"]
