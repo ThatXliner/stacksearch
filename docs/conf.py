d95b5f28ee51898ba11bc1e9c31c3bba9052db54
theXliner <66848002+ThatXliner@users.noreply.github.com>
2020-08-21 20:46:50 -0700

    sysly

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Bryan Hu"
 author = "Bryan Hu"
 
 # The full version, including alpha/beta/rc tags
-release = "1.2.15.alpha"
+release = "1.2.16.alpha"
 
 
 # -- General configuration ---------------------------------------------------
--- stacksearch/__init__.py
+++ stacksearch/__init__.py
@@ -9,6 +9,6 @@ parsing, then returns a python dict containing questions and answers.
 
 """
 
-__version__ = "1.2.15"
+__version__ = "1.2.16"
 
 __author__ = ["Bryan Hu"]
