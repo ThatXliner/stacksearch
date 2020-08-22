a7371a64206de22ccd98cb26977b8ae45da48387
theXliner <66848002+ThatXliner@users.noreply.github.com>
2020-08-22 15:40:40 -0700

    Yeet

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Bryan Hu"
 author = "Bryan Hu"
 
 # The full version, including alpha/beta/rc tags
-release = "1.2.16.alpha"
+release = "1.2.17.alpha"
 
 
 # -- General configuration ---------------------------------------------------
