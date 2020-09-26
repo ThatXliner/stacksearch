ca9ff8bdb429840375d48e6c22314e75b28cc206
ThatXliner <ThatXliner@users.noreply.github.com>
2020-09-26 17:33:29 +0000

    :robot: :art: :black_circle: -> Blackened files! [skip travis]

--- stacksearch/Search.py
+++ stacksearch/Search.py
@@ -175,7 +175,10 @@ async def fSearch(
         }
 
     async def rget(client, site):
-        return await client.get(site, timeout=5,)
+        return await client.get(
+            site,
+            timeout=5,
+        )
 
     async def s(content):
 
